e7c50d868780e361f265b382a462acdb99928994
natalieg <natalie.gornicki@gmail.com>
2015-09-02 16:06:01 +0200

    Update! Update! Update!

--- backend/productManagement.js
+++ backend/productManagement.js
@@ -2,7 +2,7 @@
 DB.connect("http://baum123.baqend.com");
 
 //Let's create a Product item
-var newProduct = function() {
+var newProduct = function () {
     var myProduct = new DB.Product();
     myProduct.name = "My Todo";
     myProduct.beschreibung = "Testbeschreibung";
@@ -11,26 +11,51 @@ var newProduct = function() {
     printItem(myProduct);
 };
 
-// Lädt alle Produkte aus der Datenbank
-var productFind = function(){
+// Lädt alle Produkte aus der Datenbank und druckt diese
+var productFindAndPrint = function () {
     DB.Product.find()
         .ascending("name")
-        .resultList(function(result) {
-        result.forEach(function(product) {
-            console.log(product.name);
+        .resultList(function (result) {
+            result.forEach(function (product) {
+                console.log(product.name);
+            });
+            printItems("Produkte", result)
         });
-        printItems("Produkte",result)
-    });
 };
+
+// Lädt alle Produkte aus der Datenbank
+var productFind = function () {
+    return DB.Product.find()
+        .ascending("name")
+        .resultList();
+};
+
+function loadProductAndUpdate() {
+    DB.Product.find()
+        .ascending("name")
+        .resultList(function (result) {
+            productUpdate(result)
+        });
+}
+
+function productUpdate(products) {
+    products.forEach(function (product) {
+        var preis = document.getElementById(product.id).value;
+        if (preis != null && preis != product.preis) {
+            product.preis = preis;
+            product.update();
+        }
+    });
+}
 
 //hier werden die Methoden ausgeführt, wenn die Datenbank bereit ist
 DB.ready(newProduct);
-DB.ready(productFind);
+DB.ready(productFindAndPrint);
 
 
-//Boilerplate code below
+//Boilerplate code below -----------------------------------------------------------------------------------------------
 
-//Funktion für
+// Gibt ein neu eingefügtes Produkt auf der Oberfläche aus
 function printItem(product) {
     $("#hello").append(JSON.stringify(product.toJSON(true), null, "  "));
 };
@@ -41,8 +66,7 @@ function printItems(msg, products) {
     products.forEach(function (product) {
         $("#hello2 table").append("<tr><td class=" + "productTD" + ">" + (product).name + " </td> " +
             "<td class=" + "productTD" + "> Preis: " +
-            "<input type=" + "text" + " id=" + "" + (product).id +  "" + " value=" + "" + (product).preis + "" + "></input></td>" + "</tr>");
+            "<input type=" + "text" + " id=" + "" + (product).id + "" + " value=" + "" + (product).preis + "" + "></input></td>" + "</tr>");
     });
 }
-
 
