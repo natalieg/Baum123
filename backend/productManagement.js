97f5822be794ee8ec845d3e637acf59a3100c757
natalieg <natalie.gornicki@gmail.com>
2015-09-02 14:26:21 +0200

    Button eingefügt ohne Funktion zum abschicken

--- backend/productManagement.js
+++ backend/productManagement.js
@@ -39,8 +39,8 @@ function printItem(product) {
 function printItems(msg, products) {
     $("#hello2 h4").html(msg);
     products.forEach(function (product) {
-        $("#hello2 table").append("<tr><td class="productTD">" + (product).name + " </td>|| " +
-            "<td class="productTD"> Preis: " +
+        $("#hello2 table").append("<tr><td class=" + "productTD" + ">" + (product).name + " </td>|| " +
+            "<td class=" + "productTD" + "> Preis: " +
             "<input type=" + "text" + " value=" + "" + (product).preis + "" + "></input></td>" + "</tr>");
     });
 }
