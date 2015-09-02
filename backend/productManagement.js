d8733032e52aaa09b88c3bde114c5cfcfcc0aab2
natalieg <natalie.gornicki@gmail.com>
2015-09-02 12:22:34 +0200

    Laden-Funktion funktioniert, Anzeige bisher nur im log

--- backend/productManagement.js
+++ backend/productManagement.js
@@ -1,39 +1,29 @@
 //Connect
 DB.connect("http://baum123.baqend.com");
 
-
 //Let's create a Product item
-function newProduct() {
+var newProduct = function() {
     var myProduct = new DB.Product();
     myProduct.name = "My Todo";
     myProduct.beschreibung = "Testbeschreibung";
     myProduct.preis = 24;
+    myProduct.liste = "Baum";
     printItem(myProduct);
-}
+};
 
-// TODO Funktioniert noch nicht richtig!
-/**
- * Findet die Produkte
- * @returns {*}
- */
-var loadProducts = function () {
-    return DB.Product.find()
-        .resultList();
-}
 
-// TODO nicht sicher ob die Daten bisher richtig geladen werden und ob die Syntax so stimmt
-// Lädt die Daten
-function loadData() {
-    return
-    loadProducts(); //query
-    (function(products) {
-        printItem2("Produkte", products);
+var productFind = function(){
+    DB.Product.find().resultList(function(result) {
+        result.forEach(function(product) {
+            console.log(product.name);
+        });
     });
-}
+};
+
 
 //hier werden die Methoden ausgeführt, wenn die Datenbank bereit ist
 DB.ready(newProduct);
-DB.ready(loadData);
+DB.ready(productFind);
 
 
 //Boilerplate code below
@@ -43,11 +33,4 @@ function printItem(product) {
     $("#hello").append(JSON.stringify(product.toJSON(true), null, "  "));
 }
 
-// TODO Funktionier noch nicht!
-function printItem2(msg, products) {
-    $("#hello2 h4").html(msg);
-    products.forEach(function (product) {
-        $("#hello2 ul").append("<li>" + product.name + " [id:" + product.id + "]</li>");
-    });
-}
 
--- frontend/test.js
+++ frontend/test.js
@@ -5,6 +5,3 @@
 DB.connect("http://baum123.baqend.com");
 
 
-DB.Product.load('bce7aaa6-7ee5-4153-a683-88874bc66083').then(function(product) {
-    console.log(product.name);
-});
