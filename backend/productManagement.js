574d4f89c8a10d6520c003f4760f5751460e849c
DerOli <oliver.wichmann@gmx.de>
2015-09-03 12:08:12 +0200

    Anzeige Stuekzahl

--- backend/productManagement.js
+++ backend/productManagement.js
@@ -67,9 +67,15 @@ function printItem(product) {
 function printItems(msg, products) {
     $("#hello2 h4").html(msg);
     products.forEach(function (product) {
-        $("#hello2 table").append("<tr><td class=" + "productTD" + ">" + (product).name + " </td> " +
-            "<td class=" + "productTD" + "> Preis: " +
-            "<input type=" + "text" + " id=" + "" + (product).id + "" + " value=" + "" + (product).preis + "" + "></input></td>" + "</tr>");
+        $("#hello2 table").append(
+            "<tr><td class=" + "productTD" + ">" + (product).name + " </td> " +
+             "<td class=" + "productTD" + "> Preis: " +
+             "<input type=" + "text" + " id=" + "" + (product).id + "" + " value=" + "" + (product).preis + "" + "></input></td>" +
+
+             "<td class=" + "productTD" + "> Stueckzahl: </td>" +
+             "<td class=" + "productTD" + ">" + (product).stueckzahl + " </td> " +
+             "<td><input type=" + "text" + " id=" + "" + (product).id + "" + (product).preis + "" + "></input></td>" +
+            "</tr>");
     });
 }
 
