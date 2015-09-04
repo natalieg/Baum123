6f8e08d27b808fa7bb35772a66735ff4c0e63b4c
natalieg <natalie.gornicki@gmail.com>
2015-09-04 14:37:47 +0200

    simples beispiel was nicht funktioniert

--- backend/landingPage.js
+++ backend/landingPage.js
@@ -2,60 +2,23 @@
  * Created by Nat on 04.09.2015.
  */
 
-
-//Connect
-DB.connect("http://baum123.baqend.com");
-
-// zeigt die topSaleNumber meist gekauften Produkte an
-var productSelectBestSales = function () {
-    DB.Product.find()
-        .descending("Verkauf_Gesamt").limit(4)
-        .resultList(function (result) {
-            result.forEach(function (product) {
-                // console.log(product.name);
-            });
-            printItems(result)
-        });
-};
-
-
-
 $(function () {
     // Grab the template script
-    var theTemplateScript = $("#bestseller-template").html();
+    var theTemplateScript = $("#address-template").html();
 
     // Compile the template
     var theTemplate = Handlebars.compile(theTemplateScript);
 
     // Define our data object
+    var context={
+        "city": "London",
+        "street": "Baker Street",
+        "number": "221B"
+    };
 
-
-
-//hier werden die Methoden ausgef?hrt, wenn die Datenbank bereit ist
-    DB.ready(productSelectBestSales);
-
-    //Gibt die Produkte auf der Oberfl?che aus
-    function printItems(products) {
-        products.forEach(function (product) {
-                var context = {
-                    product: [
-                        {imageSrc: "<img src=\"" + product.bild + "\"></img>"},
-                        {productName: product.name}
-                    ]
-                };
-                // Pass our data to the template
-                var theCompiledHtml = theTemplate(context);
-
-                // Add the compiled html to the page
-                $(document.body).append(theCompiledHtml);
-            });
-    }
-
-
-// Pass our data to the template
+    // Pass our data to the template
     var theCompiledHtml = theTemplate(context);
 
-// Add the compiled html to the page
+    // Add the compiled html to the page
     $('.content-placeholder').html(theCompiledHtml);
-})
-;
+});
